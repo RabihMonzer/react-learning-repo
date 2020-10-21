8234022037a56059cf60b55897d47b0054d2b45b
Rabih Monzer <rabihmonzer96@gmail.com>
2020-10-21 21:44:11 +0300

    Updates

---
+++ precache-manifest.64d52022a7018e14ca0e537b2b6262a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473aa043c4175ecb698a9a0612222859",
+    "url": "/react-learning-repo/index.html"
+  },
+  {
+    "revision": "3ca7b407cd4bf8f00db0",
+    "url": "/react-learning-repo/static/css/main.f0fb4073.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/react-learning-repo/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-learning-repo/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca7b407cd4bf8f00db0",
+    "url": "/react-learning-repo/static/js/main.b417245e.chunk.js"
+  },
+  {
+    "revision": "11967af53dd300b34cdf",
+    "url": "/react-learning-repo/static/js/runtime-main.f7d1094d.js"
+  }
+]);
